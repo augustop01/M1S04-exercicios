ba9b3ec2e11c7c22237193ddfb2f6adc3e146b77
Augusto Pires <mpires.augusto@gmail.com>
2023-06-19 23:17:28 -0300

    [FIX] Erro gramatical em ex4.js

--- ex4.js
+++ ex4.js
@@ -3,4 +3,4 @@ let aluno = {
     idade: prompt('Digite a idade do aluno: ')
 };
 
-alert(`O aluno de nome ${aluno.nome} tem ${aluno.idade} anos de idade.`)
+alert(`O(a) aluno(a) de nome ${aluno.nome} tem ${aluno.idade} anos de idade.`)
