baba416abee5ca2b8db37e32988add0e51c34c74
Augusto Pires <mpires.augusto@gmail.com>
2023-06-19 23:14:15 -0300

    Adicionado ex4.js já implementado ao index.html

---
+++ ex4.js
@@ -0,0 +1,6 @@
+let aluno = {
+    nome: prompt('Digite o nome do aluno: '), 
+    idade: prompt('Digite a idade do aluno: ')
+};
+
+alert(`O aluno de nome ${aluno.nome} tem ${aluno.idade} anos de idade.`)
